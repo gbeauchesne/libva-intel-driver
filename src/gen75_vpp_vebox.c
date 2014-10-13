3a2cca229fda0df42112dd70c2ac56e6dca80a55
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2014-10-13 17:58:46 +0200

    vpp: drop internal postprocessing (I965_PP_xxx) flags.
    Exclusively use VA-API defined video processing flags, instead of yet
    redefining another set with limited use. This means that we can use
    scaling, colorspace and picture structure flags as is, without an extra
    layer of conversion.
    
    Drop I965_PP_FLAG_MCDI (motion compensated deinterlacing), which was
    never set. Dropped the associated dead code, which was never used. And,
    anyway, advanced deinterlacing modes shall go through the explcit VPP
    interfaces.
    
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- src/gen75_vpp_vebox.c
+++ src/gen75_vpp_vebox.c
@@ -86,12 +86,11 @@ VAStatus vpp_surface_convert(VADriverContextP ctx,
     return va_status;
 }
 
-VAStatus vpp_surface_scaling(VADriverContextP ctx,
-                             struct object_surface *dst_obj_surf,
-                             struct object_surface *src_obj_surf)
+static VAStatus
+vpp_surface_scaling(VADriverContextP ctx, struct object_surface *dst_obj_surf,
+    struct object_surface *src_obj_surf, uint32_t flags)
 {
     VAStatus va_status = VA_STATUS_SUCCESS;
-    int flags = I965_PP_FLAG_AVS;
 
     assert(src_obj_surf->fourcc == VA_FOURCC_NV12);
     assert(dst_obj_surf->fourcc == VA_FOURCC_NV12);
@@ -1266,7 +1265,8 @@ int hsw_veb_post_format_convert(VADriverContextP ctx,
         assert(obj_surface->fourcc == VA_FOURCC_NV12);
      
         /* first step :surface scaling */
-        vpp_surface_scaling(ctx,proc_ctx->surface_output_scaled_object, obj_surface);
+        vpp_surface_scaling(ctx, proc_ctx->surface_output_scaled_object,
+            obj_surface, 0);
 
         /* second step: color format convert and copy to output */
         obj_surface = proc_ctx->surface_output_object;
